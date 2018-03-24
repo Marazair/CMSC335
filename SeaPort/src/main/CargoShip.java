c11f634430fbb0f5a7c67effb74405e772d57c4a
Marazair <nick1022m@gmail.com>
2018-03-24 14:07:54 -0400

    loadCargo and unloadCargo for CargoShip

--- SeaPort/src/main/CargoShip.java
+++ SeaPort/src/main/CargoShip.java
@@ -2,4 +2,16 @@ package main;
 
 public class CargoShip extends Ship {
 	private double cargoValue, cargoVolume, cargoWeight;
+	
+	public void loadCargo(double value, double volume, double weight) {
+		cargoValue += value;
+		cargoVolume += volume;
+		cargoWeight += weight;
+	}
+	
+	public void unloadCargo(double value, double volume, double weight) {
+		cargoValue -= value;
+		cargoVolume -= volume;
+		cargoWeight -= weight;
+	}
 }
