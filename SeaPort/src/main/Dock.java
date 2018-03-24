b9ee4a9151ff5d3c703d2b35d8ec6dd09c64cb5b
Marazair <nick1022m@gmail.com>
2018-03-24 14:37:58 -0400

    Methods for PassengerShip and Dock

--- SeaPort/src/main/Dock.java
+++ SeaPort/src/main/Dock.java
@@ -2,4 +2,26 @@ package main;
 
 public class Dock extends Thing {
 	private Ship ship;
+	
+	public Dock() {
+		ship = null;
+	}
+	
+	public Dock(Ship ship) {
+		this.ship = ship;
+	}
+	
+	public int dockShip(Ship newShip) {
+		if(ship == null) {
+			ship = newShip;
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	
+	public void undockShip() {
+		ship = null;
+	}
 }
--- SeaPort/src/main/PassengerShip.java
+++ SeaPort/src/main/PassengerShip.java
@@ -3,4 +3,20 @@ package main;
 public class PassengerShip extends Ship {
 	private int numberOfOccupiedRooms, numberOfPassengers, numberOfRooms;
 	
+	public PassengerShip (int numberOfPassengers, int numberOfRooms, int numberOfOccupiedRooms) {
+		this.numberOfPassengers = numberOfPassengers;
+		this.numberOfRooms = numberOfRooms;
+		this.numberOfOccupiedRooms = numberOfOccupiedRooms;
+	}
+	
+	public int bookRoom(int passengers, int rooms) {
+		if(numberOfOccupiedRooms + rooms <= numberOfRooms) {
+			numberOfPassengers += passengers;
+			numberOfOccupiedRooms += rooms;
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
 }
